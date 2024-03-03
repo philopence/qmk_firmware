f9db8817697ad73f3554af344c7e0414b4e2beb5
philopence <epcroo@yeah.net>
2024-03-03 12:37:23 +0800

    swap hypr and gui keys

--- keyboards/custom/keymaps/default/keymap.c
+++ keyboards/custom/keymaps/default/keymap.c
@@ -3,7 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-//////////////////////
 enum layer_names {
     _COLMAK_DH,
     _EXT,
@@ -20,17 +19,17 @@ enum layer_names {
 #define FUN_DEL LT(_FUN, KC_DEL)
 #define SFT_BSPC RSFT_T(KC_BSPC)
 
-#define LG_A LGUI_T(KC_A)
+#define H_A HYPR_T(KC_A)
 #define LA_R LALT_T(KC_R)
 #define LC_S LCTL_T(KC_S)
 #define LS_T LSFT_T(KC_T)
-#define H_G HYPR_T(KC_G)
+#define LG_G LGUI_T(KC_G)
 
-#define H_M HYPR_T(KC_M)
+#define RG_M RGUI_T(KC_M)
 #define RS_N RSFT_T(KC_N)
 #define RC_E RCTL_T(KC_E)
 #define RA_I RALT_T(KC_I)
-#define RG_O RGUI_T(KC_O)
+#define H_O HYPR_T(KC_O)
 
 #define OSM_LGUI OSM(MOD_LGUI)
 #define OSM_LALT OSM(MOD_LALT)
@@ -43,7 +42,6 @@ enum layer_names {
 #define OSM_RSFT OSM(MOD_RSFT)
 
 #define OSM_HYPR OSM(MOD_HYPR)
-
 
 const uint16_t PROGMEM w_f_combo[] = {KC_W, KC_F, COMBO_END};
 const uint16_t PROGMEM f_p_combo[] = {KC_F, KC_P, COMBO_END};
@@ -81,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-         LG_A,    LA_R,    LC_S,    LS_T,     H_G,                          H_M,    RS_N,    RC_E,    RA_I,    RG_O,
+          H_A,    LA_R,    LC_S,    LS_T,    LG_G,                         RG_M,    RS_N,    RC_E,    RA_I,     H_O,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
          KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
@@ -93,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
       QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, XXXXXXX,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-     OSM_LGUI,OSM_LALT,OSM_LCTL,OSM_LSFT,OSM_HYPR,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,
+     OSM_HYPR,OSM_LALT,OSM_LCTL,OSM_LSFT,OSM_LGUI,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
@@ -117,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PLUS,    KC_7,    KC_8,    KC_9, KC_ASTR,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-     OSM_LGUI,OSM_LALT,OSM_LCTL,OSM_LSFT,OSM_HYPR,                      KC_MINS,    KC_4,    KC_5,    KC_6, KC_SLSH,
+     OSM_HYPR,OSM_LALT,OSM_LCTL,OSM_LSFT,OSM_LGUI,                      KC_MINS,    KC_4,    KC_5,    KC_6, KC_SLSH,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         KC_0,    KC_1,    KC_2,    KC_3,  KC_DOT,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
@@ -129,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
         KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                      XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-        KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                     OSM_HYPR,OSM_RSFT,OSM_RCTL,OSM_RALT,OSM_RGUI,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                     OSM_RGUI,OSM_RSFT,OSM_RCTL,OSM_RALT,OSM_HYPR,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                      XXXXXXX, KC_BRID, XXXXXXX, KC_BRIU, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
